8b619d33a2747707f0278db2848697ae31d13a86
db118 <drrylbalderas@gmail.com>
2018-08-16 21:50:56 -0700

    Produce lat and long coordinates for zaxbys locations
    Signed-off-by: db118 <drrylbalderas@gmail.com>

--- zaxbys.py
+++ zaxbys.py
@@ -43,6 +43,35 @@ def get_location_list(location_url, state_name):
         state_information[index]['zip-code'] = temp2[1]
     return pandas.DataFrame(state_information).transpose()
 
+
+def get_latitude_longitude(address, city, state):
+    mapping_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    split_address = '+'.join(address.split(" "))
+    split_city = '+'.join(city.split(" "))
+    split_state = '+'.join(state.split(" "))
+    string = mapping_url + split_address + ",+" + split_city + ",+" + split_state
+    response = requests.get(string).json()
+    try:
+        location = response['results'][0]['geometry']['location']
+        return location['lat'], location['lng']
+    except:
+        return None, None
+
+
+def create_list_of_locations(state_informations):
+    """
+    Writes to a specified csv file
+    """
+    df = pandas.DataFrame(columns=['latitude', 'longitude'])
+    for key in state_informations.index:
+        lat, lng = get_latitude_longitude(
+            state_informations.iloc[key]['address'], state_informations.iloc[key]['city'], state_informations.iloc[key]['state'])
+        if lat == None and lng == None:
+            pass
+        else:
+            df.loc[key] = [lat, lng]
+    return df
+
 def main():
     """
     Scrapes state data for the amount of zaxbys in the state
@@ -54,8 +83,8 @@ def main():
     state_names = get_state_names(location_url)
     for state_name in state_names:
         state_information = get_location_list(location_url, state_name)
-        df = pandas.concat([df, state_information])
-    df.to_csv('location_info.csv', index=False)
+        df = pandas.concat([df, create_list_of_locations(state_information)])
+    df.to_csv('coordinates.csv', index=False)
 
 
 
